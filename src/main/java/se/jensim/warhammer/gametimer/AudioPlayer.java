762c94de2a89583c69f2c5715ab099b22de42aa2
Jens Brimberg <jens.brimberg@gmail.com>
2014-03-04 22:59:23 +0100

    Placed the playing of audiofiles each in a separate thread, this prevents the audioplayer from locking the guithread

--- src/main/java/se/jensim/warhammer/gametimer/AudioPlayer.java
+++ src/main/java/se/jensim/warhammer/gametimer/AudioPlayer.java
@@ -14,58 +14,72 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 public class AudioPlayer {
-    private final int BUFFER_SIZE = 128000;
-    private InputStream soundFile;
-    private AudioInputStream audioStream;
-    private AudioFormat audioFormat;
-    private SourceDataLine sourceLine;
 
-    /**
-     * @param filename the name of the file that is going to be played
-     */
-    public void playSound(String filename){
+	/**
+	 * @param filename
+	 *            the name of the file that is going to be played
+	 */
+	public void playSound(String filename) {
+		Thread t = new PlayerClass(filename);
+		t.start();
+	}
 
-        try {
-        	soundFile = new BufferedInputStream(ClassLoader.getSystemResourceAsStream(filename));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	private class PlayerClass extends Thread {
 
-        try {
-            audioStream = AudioSystem.getAudioInputStream(soundFile);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+		private final int BUFFER_SIZE = 128000;
+		private InputStream soundFile;
+		private AudioInputStream audioStream;
+		private AudioFormat audioFormat;
+		private SourceDataLine sourceLine;
+		private final String filename;
 
-        audioFormat = audioStream.getFormat();
+		public PlayerClass(String file) {
+			this.filename = file;
+		}
 
-        DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
-        try {
-            sourceLine = (SourceDataLine) AudioSystem.getLine(info);
-            sourceLine.open(audioFormat);
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		public void run() {
+			try {
+				soundFile = new BufferedInputStream(ClassLoader.getSystemResourceAsStream(filename));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
-        sourceLine.start();
+			try {
+				audioStream = AudioSystem.getAudioInputStream(soundFile);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
-        int nBytesRead = 0;
-        byte[] abData = new byte[BUFFER_SIZE];
-        while (nBytesRead != -1) {
-            try {
-                nBytesRead = audioStream.read(abData, 0, abData.length);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (nBytesRead >= 0) {
-                @SuppressWarnings("unused")
-                int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
-            }
-        }
+			audioFormat = audioStream.getFormat();
 
-        sourceLine.drain();
-        sourceLine.close();
-    }
+			DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+			try {
+				sourceLine = (SourceDataLine) AudioSystem.getLine(info);
+				sourceLine.open(audioFormat);
+			} catch (LineUnavailableException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			sourceLine.start();
+
+			int nBytesRead = 0;
+			byte[] abData = new byte[BUFFER_SIZE];
+			while (nBytesRead != -1) {
+				try {
+					nBytesRead = audioStream.read(abData, 0, abData.length);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				if (nBytesRead >= 0) {
+					@SuppressWarnings("unused")
+					int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
+				}
+			}
+
+			sourceLine.drain();
+			sourceLine.close();
+		}
+	}
 }
